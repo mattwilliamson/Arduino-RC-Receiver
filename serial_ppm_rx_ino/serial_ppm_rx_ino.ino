7c530137887a4c88bb22fc42188d534d0f904c72
Matt Williamson <matt@teltechcorp.com>
2012-05-02 13:08:53 -0400

    Revert "Added link to Arduino IDE"
    This reverts commit 693ac97f6ae42f2efc38a6a0f0e206085000b226.

--- serial_ppm_rx_ino/serial_ppm_rx_ino.ino
+++ serial_ppm_rx_ino/serial_ppm_rx_ino.ino
@@ -1,30 +1,17 @@
-#include <SoftwareSerial.h>
+#include <TimerOne.h> 
 
 #define CHANNELS 8
 
 // Set these a little high since they are registering low on AQ
-#if defined(__AVR_ATtiny85__)
-  // Set up pulse lengths with delays appropriate for ATtiny85 8MHz
-  #define MIN_PULSE_TIME 1030   // 1000us, compensated for delay
-  #define MAX_PULSE_TIME 1990   // 2000us, compensated for delay
-  #define HALF_PULSE_TIME (MIN_PULSE_TIME + MAX_PULSE_TIME) / 2
-  #define SYNC_PULSE_TIME 3050  // 3000us, compensated for delay
-#else
-  // ATmega328 16MHz
-  #define MIN_PULSE_TIME 1030   // 1000us, compensated for delay
-  #define MAX_PULSE_TIME 1990   // 2000us, compensated for delay
-  #define HALF_PULSE_TIME (MIN_PULSE_TIME + MAX_PULSE_TIME) / 2
-  #define SYNC_PULSE_TIME 3050  // 3000us, compensated for delay
-#endif
+#define MIN_PULSE_TIME 1030   // 1000us
+#define MAX_PULSE_TIME 1990   // 2000us
+#define HALF_PULSE_TIME (MIN_PULSE_TIME + MAX_PULSE_TIME) / 2
+#define SYNC_PULSE_TIME 3050  // 3000us
 
 #define SERIAL_BAUD 38400
 
-#define PIN_LED 4 // ATtiny85 pin 3
-#define PIN_PPM 3 // ATtiny pin 2
-
-SoftwareSerial rxSerial(0, 1); // ATtiny85 pins 6 & 7
-
-#define SERIAL rxSerial
+#define PIN_LED 13
+#define PIN_PPM 9
 
 #define RECEIVER_TIMEOUT 1500 // 1.5s
 #define MIN_RECEIVER_VALUE 0
@@ -54,26 +41,31 @@ byte currentByte;
 unsigned long lastReceived = 0;
 boolean armed = false;
 
-// Sync pulse first
-int currentChannel = CHANNELS - 1;
-unsigned long currentPulse = SYNC_PULSE_TIME;
-unsigned long lastPulseMicros = 0;
-
-unsigned long lastLedBlink = 0;
-boolean ledOn = false;
-
-
 void setDefaultPulseWidths() {
   for (int i=0; i<CHANNELS; i++) {
     pulseWidths[i] = defaultPulseWidths[i];
   }
 }
 
-void handleSerial() {
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_PPM, OUTPUT);
+
+  Serial.begin(38400);
+  
+  setDefaultPulseWidths();
+  
+  // Start timer with sync pulse
+  Timer1.initialize(SYNC_PULSE_TIME);
+  Timer1.attachInterrupt(isr_sendPulses);
+  isr_sendPulses();
+}
+
+void loop() {
   // Handle Serial Data
-  if (SERIAL.available()) {
+  if (Serial.available()) {
     lastReceived = millis();
-    currentByte = SERIAL.read();
+    currentByte = Serial.read();
 
     if (currentByte == 254) {
       // Either packet is done, or we got corrupt data. Reset the packet
@@ -94,31 +86,23 @@ void handleSerial() {
       }
     }
   }
-}
 
-void checkArmed() {
   if(!armed) {
     // Not Armed yet (no packets received) or lost reception, blink the LED
     // and set the dault positions
     setDefaultPulseWidths();
     
-    if(lastLedBlink == 0 || (millis() - lastLedBlink) >= 500) {
-      lastLedBlink = millis();
-      ledOn = !ledOn;
-      
-      if(ledOn)
-        digitalWrite(PIN_LED, HIGH);
-      else
-        digitalWrite(PIN_LED, LOW);
-    }
+    digitalWrite(PIN_LED, HIGH);
+    delay(300);
+    
+    digitalWrite(PIN_LED, LOW);
+    delay(300);
   } else {
     digitalWrite(PIN_LED, HIGH);
   }
-}
-
-void checkLostReception() {
+  
   // Check if we lost reception
-  if(armed && lastReceived > 0 && millis() - lastReceived > RECEIVER_TIMEOUT || millis() - lastReceived + 10 > RECEIVER_TIMEOUT) {
+  if(armed && lastReceived > 0 &&  millis() - lastReceived > RECEIVER_TIMEOUT) {
     armed = false;
     
     for(int i=0; i<5; i++) {
@@ -128,41 +112,24 @@ void checkLostReception() {
       delay(100);
     }
   }
-}
+} 
 
-void sendPulses() {
-  if((micros() - lastPulseMicros) >= currentPulse) {
-    digitalWrite(PIN_PPM, LOW);
-    
-    currentChannel++;
-    
-    if (currentChannel == CHANNELS) {
-      // After last channel
-      currentPulse = SYNC_PULSE_TIME;
-      currentChannel = -1; // Will be 0 on next interrupt
-    } else {
-      currentPulse = pulseWidths[currentChannel];
-    }
-    
-    digitalWrite(PIN_PPM, HIGH);
-    lastPulseMicros = micros();
-  }
-}
+// Sync pulse first
+volatile int currentChannel = CHANNELS - 1;
 
-void setup() {
-  pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN_PPM, OUTPUT);
-
-  SERIAL.begin(38400);
+void isr_sendPulses() {
+  digitalWrite(PIN_PPM, LOW);
   
-  setDefaultPulseWidths();
-  sendPulses();
-}
-
-void loop() {
-  handleSerial();
-  checkArmed();
-  checkLostReception();
-  sendPulses();
+  currentChannel++;
+  
+  if (currentChannel == CHANNELS) {
+    // After last channel
+    Timer1.setPeriod(SYNC_PULSE_TIME);
+    currentChannel = -1; // Will be 0 on next interrupt
+  } else {
+    Timer1.setPeriod(pulseWidths[currentChannel]);
+  }
+  
+  digitalWrite(PIN_PPM, HIGH);
 }
 
